ab831fc636193477205274ed79f087c5ff03d9b2
Stefano Fontanelli <s.fontanelli@gmail.com>
2015-03-09 23:20:42 -0700

    Fix build.sh to push all images not latest only.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 docker build --no-cache=true -t stefanofontanelli/cassandra .
 docker push stefanofontanelli/cassandra:latest
 docker tag -f stefanofontanelli/cassandra:latest stefanofontanelli/cassandra:2.1.3
+docker push stefanofontanelli/cassandra:2.1.3
