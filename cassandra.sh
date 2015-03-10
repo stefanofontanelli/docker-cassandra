4d237bbbaeb08e9581fc83fa1a9f4431ebf4a9f8
Stefano Fontanelli <s.fontanelli@gmail.com>
2015-03-10 16:26:08 -0700

    Fix image.

--- cassandra.sh
+++ cassandra.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 ADDR=$(ip addr | grep 'inet ' | grep eth0 | cut -d/ -f1 | awk '{print $2}')
-sed -i "s/listen_address: localhost/listen_address: $ADDR/g" /etc/cassandra/cassandra.yaml
-sed -i "s/rpc_address: localhost/rpc_address: 0.0.0.0/g" /etc/cassandra/cassandra.yaml
-sed -i "s/# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: $ADDR/g" /etc/cassandra/cassandra.yaml
-sed -i 's/#MAX_HEAP_SIZE="4G"/MAX_HEAP_SIZE="4G"/g' /etc/cassandra/cassandra-env.sh
-sed -i 's/#HEAP_NEWSIZE="800M"/HEAP_NEWSIZE="800M"/g' /etc/cassandra/cassandra-env.sh
+
+echo "Cassandra will bind to $ADDR"
+
+sed -i -e "s/listen_address: localhost/listen_address: $ADDR/g" /etc/cassandra/cassandra.yaml
+cat /etc/cassandra/cassandra.yaml | grep "listen_address"
+
+sed -i -e "s/rpc_address: localhost/rpc_address: $ADDR/g" /etc/cassandra/cassandra.yaml
+cat /etc/cassandra/cassandra.yaml | grep "rpc_address"
+
+sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$ADDR\"/g" /etc/cassandra/cassandra.yaml
+cat /etc/cassandra/cassandra.yaml | grep "seeds:"
+
 cassandra -f
