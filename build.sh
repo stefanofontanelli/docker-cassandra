90bcfb39e8c8f9b51ec7474e59c70acade2f21ce
Giacomo Bagnoli <gbagnoli@gmail.com>
2015-07-09 14:40:32 +0100

    Upgrade to 2.1.7

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
+VERSION='2.1.7'
 docker build --no-cache=true -t stefanofontanelli/cassandra .
 docker push stefanofontanelli/cassandra:latest
-docker tag -f stefanofontanelli/cassandra:latest stefanofontanelli/cassandra:2.1.3
-docker push stefanofontanelli/cassandra:2.1.3
+docker tag -f stefanofontanelli/cassandra:latest stefanofontanelli/cassandra:$VERSION
+docker push stefanofontanelli/cassandra:$VERSION
