999e6bd2d95c1e45237a132ed15dd5f78912497a
Giacomo Bagnoli <gbagnoli@gmail.com>
2015-09-02 21:16:10 +0100

    DRY, remove useless cat usage

--- cassandra.sh
+++ cassandra.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ADDR=$(ip addr | grep 'inet ' | grep eth0 | cut -d/ -f1 | awk '{print $2}')
+CONF=/etc/cassandra/cassandra.yaml
 
 echo "Cassandra will bind to $ADDR"
 
-sed -i -e "s/listen_address: localhost/listen_address: $ADDR/g" /etc/cassandra/cassandra.yaml
-cat /etc/cassandra/cassandra.yaml | grep "listen_address"
+sed -i -e "s/listen_address: localhost/listen_address: $ADDR/g" $CONF
+grep "listen_address" $CONF
 
-sed -i -e "s/rpc_address: localhost/rpc_address: $ADDR/g" /etc/cassandra/cassandra.yaml
-cat /etc/cassandra/cassandra.yaml | grep "rpc_address"
+sed -i -e "s/rpc_address: localhost/rpc_address: $ADDR/g" $CONF
+grep "rpc_address" $CONF
 
-sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$ADDR\"/g" /etc/cassandra/cassandra.yaml
-cat /etc/cassandra/cassandra.yaml | grep "seeds:"
+sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$ADDR\"/g" $CONF
+grep "seeds:" $CONF
 
 cassandra -f
